5d26ade4e707e677e499e906f54fc2bb037f02af
Sebastian <sebastiansorich@gmail.com>
2024-06-24 17:30:23 -0400

    Backend final finalisimo

--- src/pages/Homepage.controllers.js
+++ src/pages/Homepage.controllers.js
@@ -7,20 +7,20 @@ const HomePage = async (req, res) => {
     <body>
         <h1>Index</h1>
         <h2>Controladores</h2>
-        <p>Proveedores: <a href="http://localhost:3000/proveedores">http://localhost:3000/proveedores</a></p>
-        <p>Productos: <a href="http://localhost:3000/productos">http://localhost:3000/productos</a></p>
-        <p>Clientes: <a href="http://localhost:3000/clientes">http://localhost:3000/clientes</a></p>
-        <p>Trabajadores: <a href="http://localhost:3000/trabajadores">http://localhost:3000/trabajadores</a></p>
-        <p>Cargos: <a href="http://localhost:3000/cargos">http://localhost:3000/cargos</a></p>
-        <p>NIT: <a href="http://localhost:3000/nit">http://localhost:3000/nit</a></p>
-        <p>Notas de Entrega: <a href="http://localhost:3000/notasEntrega">http://localhost:3000/notasEntrega</a></p>
-        <p>Pagos: <a href="http://localhost:3000/pagos">http://localhost:3000/pagos</a></p>        
-        <p>Facturas de Venta: <a href="http://localhost:3000/facturas">http://localhost:3000/facturas</a></p>
-        <p>Notas de Pedido: <a href="http://localhost:3000/NotaPEdidoCompra/">http://localhost:3000/NotaPEdidoCompra/</a></p>
+        <p>Proveedores: <a href="https://backend-oasis.onrender.com//proveedores">https://backend-oasis.onrender.com//proveedores</a></p>
+        <p>Productos: <a href="https://backend-oasis.onrender.com//productos">https://backend-oasis.onrender.com//productos</a></p>
+        <p>Clientes: <a href="https://backend-oasis.onrender.com//clientes">https://backend-oasis.onrender.com//clientes</a></p>
+        <p>Trabajadores: <a href="https://backend-oasis.onrender.com//trabajadores">https://backend-oasis.onrender.com//trabajadores</a></p>
+        <p>Cargos: <a href="https://backend-oasis.onrender.com//cargos">https://backend-oasis.onrender.com//cargos</a></p>
+        <p>NIT: <a href="https://backend-oasis.onrender.com//nit">https://backend-oasis.onrender.com//nit</a></p>
+        <p>Notas de Entrega: <a href="https://backend-oasis.onrender.com//notasEntrega">https://backend-oasis.onrender.com//notasEntrega</a></p>
+        <p>Pagos: <a href="https://backend-oasis.onrender.com//pagos">https://backend-oasis.onrender.com//pagos</a></p>        
+        <p>Facturas de Venta: <a href="https://backend-oasis.onrender.com//facturas">https://backend-oasis.onrender.com//facturas</a></p>
+        <p>Notas de Pedido: <a href="https://backend-oasis.onrender.com//NotaPEdidoCompra/">https://backend-oasis.onrender.com//NotaPEdidoCompra/</a></p>
         <br></br>
         <h2>Generacion de reportes</h2>
         <br></br>
-        <p>Reportes de inventario: <a href="http://localhost:3000/Reportes/generar_inventario">http://localhost:3000/Reportes/generar_inventario/</a></p>
+        <p>Reportes de inventario: <a href="https://backend-oasis.onrender.com//Reportes/generar_inventario">https://backend-oasis.onrender.com//Reportes/generar_inventario/</a></p>
     </body>
     </html>
   `);
